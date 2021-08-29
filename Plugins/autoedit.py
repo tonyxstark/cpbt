4c65fafb9cf363d322a3f56a12959e00d7c9fa86
Mr. Stark <75563336+tonyxstark@users.noreply.github.com>
2021-08-29 10:25:33 +0530

    Update autoedit.py

--- Plugins/autoedit.py
+++ Plugins/autoedit.py
@@ -25,7 +25,7 @@ async def editing(bot, message):
          pass 
       if (message.document or message.video or message.audio): 
           if message.caption:                        
-             file_caption = f"**{message.caption}**"                
+             file_caption = f"`{message.caption}`"                
           else:
              fname = media.file_name
              filename = fname.replace("_", " ")
